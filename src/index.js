b504c23367b0bb5950ec1b28dfd0b1082ad3abf2
MilaKoseckaya <kosetsaya@gmail.com>
2025-10-31 23:39:39 +0200

    init messages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { greetingMessage, exitMessage } from './messages.js';
 
+const DEFAULT_USERNAME = 'Anonymous';
+
+const args = process.argv.slice(2);
+let usernameArg = args.find((el) => el.startsWith('--username'));
+const username = usernameArg
+    ? usernameArg.replace('--username=', '').trim()
+    : DEFAULT_USERNAME;
+
+greetingMessage(username);
+
+process.on('exit', () => exitMessage(username));
--- src/messages.js
+++ src/messages.js
@@ -1,1 +1,19 @@
+export const greetingMessage = (username) => {
+    console.log(`Welcome to the File Manager, ${username}!`);
+};
 
+export const exitMessage = (username) => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+};
+
+export const currentDirectory = () => {
+    console.log('You are currently in', process.cwd());
+};
+
+export const invalidInput = () => {
+    console.log('Invalid input');
+};
+
+export const operationError = () => {
+    console.log('Operation failed');
+};
